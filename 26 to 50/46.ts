7d7e6fb13fe02e6aced2b3f58c850fe1dfb3bbd9
ashish-simform <ashish@simformsolutions.com>
2024-01-13 11:22:21 +0530

    added 46 and 47 example

--- 26 to 50/46.ts
+++ 26 to 50/46.ts
@@ -10,4 +10,25 @@ const result1 = returnsValueOnly("a")
 // ^? const result1: "a"
 
 //when returning an object or array, it doesn't infer the .literal type
-// const reut 136
+const returnsValueInAnObject = <T1> (t: T1) => {
+    return {
+        t
+    }
+}
+
+const result2 = returnsValueInAnObject("abc")
+// ^? const result2: {
+//   t: string;
+// };
+
+//with a constraint, it narrows it to its literal
+const returnsValuesInAnObjectWithConstraint = <T1 extends string>(t: T1) => {
+    return {
+        t
+    }
+}
+
+const result3 = returnsValuesInAnObjectWithConstraint("ak")
+// ^? const result3: {
+//      t: "ak";
+//    }
---
+++ 26 to 50/47.ts
@@ -0,0 +1,42 @@
+//literal inference on arguments explainer
+
+//When returning the value only it infers the literal type
+
+const acceptsValueOnly = <T>(t: T) => {
+    return t;
+}
+
+const r1 = acceptsValueOnly("a")
+// ^? const result: "a"
+
+const acceptsValueInAnObject = <T>(obj: {input: T}) => {
+    return obj.input;
+}
+
+const r2 = acceptsValueInAnObject({input: "ashish"})
+// ^? const r2: string
+
+const r3 = acceptsValueInAnObject({ input: "abc"} as const);
+// ^? const r3: "abc"
+
+const acceptsValueInAnObjectFieldWithConstraint = <T extends string>(obj: {input: T}) => {
+    return obj.input;
+}
+
+const r4 = acceptsValueInAnObjectFieldWithConstraint({input: "ak"})
+// ^? const r4: "ak"
+
+const acceptsValueWithObjectConstraint = <T extends {input: string}>(obj: T) => {
+    return obj.input;
+}
+
+const r5 = acceptsValueWithObjectConstraint({ input: "ashish"})
+// ^? const r5: string
+
+const r5WithAsConst = acceptsValueWithObjectConstraint({
+    input: "ashish"
+} as const)
+// ^? const r5: string
+
+
+
